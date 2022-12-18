17618e5256a5b301ee1ff280c096f6b93e284f10
Brahim Amrouch <amroucheb4@gmail.com>
2022-12-19 00:31:52 +0100

    Parsing done, commands incomming

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: bamrouch <bamrouch@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 08:17:12 by bamrouch          #+#    #+#             */
-/*   Updated: 2022/12/18 04:15:32 by bamrouch         ###   ########.fr       */
+/*   Updated: 2022/12/18 23:27:46 by bamrouch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@
 
 typedef struct s_stack_group
 {
-    t_list a;
-    t_list b;
+    t_list  *a;
+    t_list  *b;
 }   t_stack_group;
 
-t_boolean ft_manage_parser(char *input[]);
+void    ft_init_stack_group(t_list **stack, int input_len, char *input_content[]);
 
 #endif 
--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@ int main(int argc,char *argv[])
     (void) argc;
     t_stack_group stacks;
     ft_bzero(&stacks,sizeof(t_stack_group));
-    ft_init_stack_group(  argv);
+    ft_init_stack_group(&(stacks.a), argc, argv);
     ft_printf("main program");
     return (0);
 }
