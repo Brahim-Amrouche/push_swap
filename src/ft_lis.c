68b084dd8e8f56a3604f839df31428fb33d6da55
Brahim Amrouch <amroucheb4@gmail.com>
2022-12-28 22:48:25 +0100

    what in hell is this dark magic im writting

--- src/ft_lis.c
+++ src/ft_lis.c
@@ -6,7 +6,7 @@
 /*   By: bamrouch <bamrouch@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/26 16:17:03 by bamrouch          #+#    #+#             */
-/*   Updated: 2022/12/26 20:33:51 by bamrouch         ###   ########.fr       */
+/*   Updated: 2022/12/28 22:48:10 by bamrouch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 t_boolean   ft_is_smallest_lis_element(int **lis,int value)
 {
+    // If smallest element in lis end elements create a new lis with len 1;
     int i;
     int sub_lis_len;
 
@@ -24,12 +25,13 @@ t_boolean   ft_is_smallest_lis_element(int **lis,int value)
         if (value > lis[i++][sub_lis_len - 1])
             return FALSE;
     }   
-    ft_malloc_sub_lis(lis,1,value);
+    ft_malloc_sub_lis(lis,2,value);
     return TRUE;
 }
 
 t_boolean   ft_is_largest_lis_element(int ***lis, int value)
 {
+    
     int i;
     int sub_lis_len;
     
@@ -39,50 +41,52 @@ t_boolean   ft_is_largest_lis_element(int ***lis, int value)
         sub_lis_len = lis[i][0];
         if(value < lis[i][sub_lis_len - 1])
         {
-            ft_malloc_sub_lis(lis,i - 1,value);
+            // if not biggest lis end element replace with the one smaller than it
+            ft_malloc_sub_lis(lis,i,value);
             return FALSE;
         }
         i++;
     }
+    // If biggest lis end element add it to the longest existing lis
     ft_malloc_sub_lis(lis,(*lis)[0][0],value);
     return TRUE;
+}
+
+void    ft_push_new_lis(int ***lis ,int list_len,int value)
+{
+    int *new_sub_lis;
+    int **new_lis;
+    int i;
+
+    new_lis = malloc(((*lis)[0][0] + 1) * sizeof(int *));
+    if (!new_lis)
+        return
+    i = -1;
+    while (++i < (*lis)[0][0])
+        new_lis[i] = (*lis)[i];
+    free(*lis);
+    new_lis[0][0]++;
+    new_sub_lis = malloc((list_len + 1) * sizeof(int));
+    if (!new_sub_lis)
+        return;
+    i = -1;
+    while (++i < list_len)
+        new_sub_lis[i] = new_lis[new_lis[0][0] - 2][i];
+    new_lis[new_lis[0][0] - 1] = new_sub_lis;
+    new_sub_lis[list_len] = value;
+    new_sub_lis[0]++;
+    *lis = new_lis;
 }
 
 void    ft_malloc_sub_lis(int ***lis,int index,int value)
 {
     int list_len;
-    int *new_sub_lis;    
-    int **new_lis;
-    int i;
 
     list_len = (*lis)[index - 1][0];
     if (index == (*lis)[0][0])
-    {
-        new_lis = malloc(sizeof(int *) * ((*lis)[0][0] + 1));
-        if (!new_lis)
-            return;
-        i = 0;
-        while(i < (*lis)[0][0])
-        {
-            new_lis[i] = (*lis)[i];
-            i++;
-        }
-        free(*lis);
-        new_sub_lis = malloc(sizeof(int) * (list_len + 1));
-        if (!new_sub_lis)
-            return;
-        new_lis[index] = new_sub_lis;
-        new_lis[0][0]++;
-        i = 0;
-        while (i < list_len)
-        {
-            new_sub_lis[i]= new_lis[index - 1][i];
-            i++;
-        }
-        new_sub_lis[i] = value;
-        new_sub_lis[0]++;
-        *lis = new_lis;
-    }   
+        ft_push_new_lis(lis, list_len, value);
+    else
+        (*lis)[index - 1][list_len - 1] = value;
 }
 
 int *ft_find_lis(t_list *stack_a)
@@ -90,9 +94,9 @@ int *ft_find_lis(t_list *stack_a)
     int temp;
     int **lis;
 
-    lis = malloc(2 * sizeof(int *));
-    lis[0] = malloc(1 * sizeof(int));
-    lis[0][0] = 2;
+    lis = malloc(2 * sizeof(int *)); 
+    lis[0] = malloc(1 * sizeof(int)); // first lis element is the lenght of the list 
+    lis[0][0] = 2;// will have 2 elements at the start
     lis[1] = malloc(2 * sizeof(int));
     lis[1][0] = 2;
     lis[1][1] = *((int *) stack_a->content);
